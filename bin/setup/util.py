5a51f0ad2c6404329d0b1fec6748f3b3580f4bcf
sutne <sivertutne@gmail.com>
2022-07-20 10:58:09 +0200

    refac

--- bin/setup/util.py
+++ bin/setup/util.py
@@ -3,12 +3,12 @@ import sys
 
 def min_python_version(major: int, minor: int, micro: int = 0) -> None:
     """Make sure users python version is equal or higher than the minimum specified"""
-    act_major = sys.version_info.major
-    act_minor = sys.version_info.minor
-    act_micro = sys.version_info.micro
-    if act_major >= major and act_minor >= minor and act_micro >= micro:
-        return
-    sys.exit(
-        f"Python {major}.{minor}.{micro} or later is required.\
-        \nYou have {act_major}.{act_minor}.{act_micro}"
+    sys_version_up_to_date = (
+        major <= sys.version_info.major
+        and minor <= sys.version_info.minor
+        and micro <= sys.version_info.micro
     )
+    if not sys_version_up_to_date:
+        feedback = f"Python {major}.{minor}.{micro} or later is required.\n"
+        feedback += f"You have {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+        sys.exit(feedback)
